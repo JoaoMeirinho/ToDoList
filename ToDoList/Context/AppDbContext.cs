0a60d16542f92097d60109f6021f0f9a1a426f6d
João Meirinho <jao46roberto@gmail.com>
2024-04-08 20:18:00 -0300

    Adicionar arquivos de projeto.

---
+++ ToDoList/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Formats.Tar;
+
+namespace ToDoList.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TarefasModel> Tarefas { get ; set; }
+        public DbSet<UserModel> Users { get ; set; }
+    }
+
+}
---
+++ ToDoList/Models/TarefasModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.Models
+{
+    public class TarefasModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set;} = DateTime.Now;
+        public UserModel User { get; set; }
+    }
+}
